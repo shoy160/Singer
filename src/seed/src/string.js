6a442a7877765d0f11a0fb3d0c8fcf61fdb0cbab
shoy <634330628@qq.com>
2014-05-10 20:05:26 +0800

    常规修改

--- src/seed/src/string.js
+++ src/seed/src/string.js
@@ -3,79 +3,103 @@
         trim = String.prototype.trim,
         SUBSTITUTE_REG = /\\?\{([^{}]+)\}/g,
         EMPTY = '';
-    /**
-     * trim
-     * @param str
-     * @returns {*}
-     */
-    S.trim = function (str) {
-        return S.isEmpty(str) ? str : (trim ? trim.call(str) : (str + '').replace(RE_TRIM, EMPTY));
-    };
-    /**
-     * 替代
-     * @param str
-     * @param o
-     * @param regexp
-     * @returns {*}
-     */
-    S.substitute = function (str, o, regexp) {
-        if (!(S.isString(str) && o)) {
+    S._mix(S, {
+        /**
+         * trim
+         * @param str
+         * @returns {*}
+         */
+        trim: function (str) {
+            return S.isEmpty(str) ? str : (trim ? trim.call(str) : (str + '').replace(RE_TRIM, EMPTY));
+        },
+        lengthCn: function (str) {
+            if (!S.isString(str)) return 0;
+            return str.replace(/[\u4e00-\u9fa5]/g, "**").length;
+        },
+        subCn: function (str, num, strip) {
+            if (S.lengthCn(str) <= num) return str.toString();
+            for (var i = 0; i < str.length; i++) {
+                if (S.lengthCn(this.substr(0, i)) >= num) {
+                    return str.substr(0, i) + (strip || "...");
+                }
+            }
             return str;
+        },
+        stripTags: function (str) {
+            return str.replace(/<\/?[^>]+>/gi, '');
+        },
+        stripScript: function (h) {
+            return h.replace(/<script[^>]*>([\\S\\s]*?)<\/script>/g, '')
+        },
+        isMobile: function (m) {
+            return /^((0[1-9]{2,3}[\s-]?)?\d{7,8})|(1[3,5,8]\d{9})$/.test(L.trim(m))
+        },
+        /**
+         * 替代
+         * @param str
+         * @param o
+         * @param regexp
+         * @returns {*}
+         */
+        substitute: function (str, o, regexp) {
+            if (!(S.isString(str) && o)) {
+                return str;
+            }
+            return str.replace(regexp || SUBSTITUTE_REG, function (match, name) {
+                if (match.charAt(0) === '\\') {
+                    return match.slice(1);
+                }
+                return (o[name] === undefined) ? EMPTY : o[name];
+            });
+        },
+        /**
+         * 首字母大写
+         * @param str
+         * @returns {string}
+         */
+        ucFirst: function (str) {
+            str += '';
+            return str.charAt(0).toUpperCase() + str.substring(1);
+        },
+        /**
+         * 以某个字符串开始
+         * @param str
+         * @param prefix
+         * @returns {boolean}
+         */
+        startsWith: function (str, prefix) {
+            return str.lastIndexOf(prefix, 0) === 0;
+        },
+        /**
+         * 以某个字符串结束
+         * @param str
+         * @param suffix
+         * @returns {boolean}
+         */
+        endsWith: function (str, suffix) {
+            var ind = str.length - suffix.length;
+            return ind >= 0 && str.indexOf(suffix, ind) === ind;
+        },
+        /**
+         *  格式化字符串
+         * @param str
+         * @returns {*}
+         */
+        format: function (str) {
+            if (arguments.length <= 1) return str || EMPTY;
+            var result = str;
+            if (2 === arguments.length && S.isObject(arguments[1])) {
+                for (var key in arguments[1]) {
+                    var reg = new RegExp("\\{" + key + "\\}", "gi");
+                    result = result.replace(reg, arguments[1][key]);
+                }
+            } else {
+                for (var i = 1; i < arguments.length; i++) {
+                    var reg = new RegExp("\\{" + (i - 1) + "\\}", "gi");
+                    result = result.replace(reg, arguments[i]);
+                }
+            }
+            return result;
         }
-        return str.replace(regexp || SUBSTITUTE_REG, function (match, name) {
-            if (match.charAt(0) === '\\') {
-                return match.slice(1);
-            }
-            return (o[name] === undefined) ? EMPTY : o[name];
-        });
-    };
-    /**
-     * 首字母大写
-     * @param str
-     * @returns {string}
-     */
-    S.ucFirst = function (str) {
-        str += '';
-        return str.charAt(0).toUpperCase() + str.substring(1);
-    };
-    /**
-     * 以某个字符串开始
-     * @param str
-     * @param prefix
-     * @returns {boolean}
-     */
-    S.startsWith = function (str, prefix) {
-        return str.lastIndexOf(prefix, 0) === 0;
-    };
-    /**
-     * 以某个字符串结束
-     * @param str
-     * @param suffix
-     * @returns {boolean}
-     */
-    S.endsWith = function (str, suffix) {
-        var ind = str.length - suffix.length;
-        return ind >= 0 && str.indexOf(suffix, ind) === ind;
-    };
-    /**
-     *  格式化字符串
-     * @param str
-     * @returns {*}
-     */
-    S.format = function (str) {
-        if (arguments.length <= 1) return str || EMPTY;
-        var result = str;
-        if (2 === arguments.length && S.isObject(arguments[1])) {
-            for (var key in arguments[1]) {
-                var reg = new RegExp("\\{" + key + "\\}", "gi");
-                result = result.replace(reg, arguments[1][key]);
-            }
-        } else {
-            for (var i = 1; i < arguments.length; i++) {
-                var reg = new RegExp("\\{" + (i - 1) + "\\}", "gi");
-                result = result.replace(reg, arguments[i]);
-            }
-        }
-        return result;
-    };
+    });
 })(SINGER);
